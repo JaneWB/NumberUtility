11afff44c05aefa8fe8b27c8e24832039d2487d3
JaneWB <brownjannick@att.net>
2018-10-15 09:52:59 -0500

    Number Utility library for Number Cruncher

--- NumberUtility/Class1.cs
+++ NumberUtility/Class1.cs
@@ -1,8 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
 
 namespace NumberUtility
 {
-    public class Class1
+    public class NumberCruncher
     {
+        //public List<int> nums = new List<int>();
+
+        public static int SumOfEvens(List<int> nums)
+        {
+            int result = 0;
+
+            foreach (int num in nums)
+                if (num % 2 == 0)
+            {
+                  
+                  result += num;  
+            }
+               
+            return result;
+        }
     }
 }
